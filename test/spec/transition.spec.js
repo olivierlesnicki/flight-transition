a03d644d6760b0d313d6404b88e4f6affd4a973d
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2013-07-25 18:16:09 -0400

    Initial commit

---
+++ test/spec/transition.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describeComponent('lib/transition', function () {
+
+  // Initialize the component and attach it to the DOM
+  beforeEach(function () {
+    setupComponent();
+  });
+
+  it('should be defined', function () {
+    expect(this.component).toBeDefined();
+  });
+
+});
