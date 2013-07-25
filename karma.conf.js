e3b999077e7b81eba3dd46c21728b062729f6df4
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2013-07-25 19:03:11 -0400

    Modified karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
+    
   'bower_components/es5-shim/es5-shim.js',
   'bower_components/es5-shim/es5-sham.js',
 
@@ -48,11 +49,11 @@ reporters = [
 
 // web server port
 // CLI --port 9876
-port = process.env.PORT;
+port = 9876;
 
 // cli runner port
 // CLI --runner-port 9100
-runnerPort = 0;
+runnerPort = 9100;
 
 // enable / disable colors in the output (reporters and logs)
 // CLI --colors --no-colors
@@ -91,5 +92,3 @@ singleRun = false;
 // report which specs are slower than 500ms
 // CLI --report-slower-than 500
 reportSlowerThan = 500;
-
-hostname =  process.env.IP;
